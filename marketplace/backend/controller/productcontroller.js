06d650eab7a7254df83361e658f32c3a1a8d8225
Yaman <yamanmahawar10@gmail.com>
2023-11-16 14:59:15 +0530

    added market-place project

---
+++ marketplace/backend/controller/productcontroller.js
@@ -0,0 +1,21 @@
+const ProductModel = require('../model/productmodel');
+
+const createProduct = (req, res) => {
+    const product = req.body
+    const newProduct = new ProductModel(product)
+    try {
+        newProduct.save()
+        return res.status(201).json({
+            message: "product saved successfully",
+            result: newProduct,
+            count: newProduct.count
+        })
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: err.message,
+        })
+
+    }
+}
+module.exports = { createProduct }
---
+++ marketplace/backend/routes/user.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const router = express.Router()
+
+const { createUser, getAllUser } = require("../controller/userController")
+const { createcategory, getAllProducts } = require("../controller/categorycontroller")
+const { createProduct } = require("../controller/productcontroller")
+
+router.post("/create-product", createProduct)
+router.post("/create-category", createcategory)
+router.get("/get-products", getAllProducts)
+router.post("/create-user", createUser)
+router.get("/get-users", getAllUser)
+
+module.exports = router
